e0f820a8717bcc2c1ca69bfbc7085ac89d897587
MKluczniak <61352124+MKluczniak@users.noreply.github.com>
2022-12-23 01:58:12 +0100

    updated the Register.js page, register <> login

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -87,9 +87,9 @@ const Register = () => {
           Submit{" "}
         </button>
         <p>
-          {!values.isMember ? "Not a member yet?  " : "Already a member?   "}
+          {values.isMember ? "Not a member yet?  " : "Already a member?   "}
           <button type="button" onClick={toggleMember} className="member-btn">
-            {!values.isMember ? "Register" : "Login"}
+            {values.isMember ? "Register" : "Login"}
           </button>
         </p>
       </form>
